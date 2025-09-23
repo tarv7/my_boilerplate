1c0015e5b217fd3c42805efd4fa15625df336245
Thales Augusto <thallles.ramos@gmail.com>
2025-09-22 22:44:01 -0300

    Add helper template on scaffold

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def current_sort?(attribute)
+    params[:sort] == attribute.to_s
+  end
+
+  def current_direction
+    params[:direction] == "desc" ? "desc" : "asc"
+  end
+
+  def next_direction(attribute)
+    return "desc" if current_sort?(attribute) && current_direction == "asc"
+
+    "asc"
+  end
+
+  def sort_params(attribute)
+    params.except(:controller, :action).permit!.to_h.merge(sort: attribute, direction: next_direction(attribute))
+  end
 end
